b64eafb1792d64e27a166ac76781e2d2e45f02b0
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2023-10-17 20:26:46 -0700

    Added second route using route parameters to setup template variables

--- express_server.js
+++ express_server.js
@@ -25,6 +25,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 //MESSAGE for console after startup
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
