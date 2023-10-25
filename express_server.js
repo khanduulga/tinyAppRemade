45dd02cedda488e08a5a8659711a9ef4e2f8cb88
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2023-10-24 19:07:15 -0700

    Added check for http in URL entries on new and edit cases

--- express_server.js
+++ express_server.js
@@ -19,26 +19,47 @@ app.use(express.urlencoded({ extended: true }));
 //PAGES
 
 //GET
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
-})
-
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
-  res.redirect(longURL);
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+})
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  console.log(req.params.id)
+  res.redirect(longURL);
+});
+
 //POST
+
+//helper(s) for post
+//generates a random 6 character string
+const generateRandomString = () => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let randomId = ""
+  for (i = 0; i < 6; i++) {
+    randomId += characters.charAt(Math.floor(characters.length * Math.random()))
+  }
+  return randomId
+}
+
+app.post("/urls", (req, res) => {
+  const urlId = generateRandomString()
+  const longURL = req.body.longURL
+  urlDatabase[urlId] = longURL
+  console.log(urlDatabase)
+  res.redirect("/urls/" + urlId);
+})
+
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id]
   res.redirect("/urls");
@@ -50,20 +71,8 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls")
 })
 
-const generateRandomString = () => {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-  let randomId = ""
-  for (i = 0; i < 6; i++) {
-    randomId += characters.charAt(Math.floor(characters.length * Math.random()))
-  }
-  return randomId
-}
-app.post("/urls", (req, res) => {
-  const urlId = generateRandomString()
-  urlDatabase[urlId] = req.body.longURL
 
-  res.redirect("/urls/" + urlId);
-})
+
 
 //MESSAGE for console after startup
 app.listen(PORT, () => {
