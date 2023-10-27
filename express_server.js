d3d5cfb08ff7c356b7badcc614e3dca524ba4bf8
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2023-10-26 21:25:21 -0700

    Added logout button and functionality to clear cookie

--- express_server.js
+++ express_server.js
@@ -81,6 +81,12 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 })
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect("/urls");
+})
+
+
 
 
 //MESSAGE for console after startup
